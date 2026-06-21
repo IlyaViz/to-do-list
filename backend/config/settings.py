dde0a9ddf1c0e255082ed9ac482814f2c80df899
Ilya <Bezzubik12345@i.ua>
2026-06-21 11:03:30 +0300

    cicd: first try for ci and cd (continious delivery)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = [os.getenv("FRONTEND_HOST")]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
 
 # Application definition
 
@@ -79,11 +79,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "todo_list"),
-        "USER": os.getenv("DB_USER", "todo_user"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "todo_password"),
-        "HOST": os.getenv("DB_HOST", "db"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("POSTGRES_DB", "todo_list"),
+        "USER": os.getenv("POSTGRES_USER", "todo_user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "todo_password"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
